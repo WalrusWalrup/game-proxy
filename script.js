b9198d533fa55f2f26ebe552f9b5fabcd6cc18fb
WalrusWalrup <137485792+WalrusWalrup@users.noreply.github.com>
2024-09-16 19:48:17 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-window.addEventListener('load', function() {
-    // Initialize Ruffle
-    var ruffle = window.RufflePlayer.newest();
-    var container = document.getElementById('game-container');
-    var player = ruffle.createPlayer();
-    container.appendChild(player);
 
-    // Use CORS proxy to load the SWF file
-    player.load('https://cors-anywhere.herokuapp.com/https://github.com/spencer112/Unblocked-Flash-Games/raw/master/happyWheels.swf');
-});

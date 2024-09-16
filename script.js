b79469e92b77550de750c359e0b675d9e9b1c5cc
WalrusWalrup <137485792+WalrusWalrup@users.noreply.github.com>
2024-09-16 16:25:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-    // URL of the original game content
-    const targetUrl = 'https://1v1lolonline.github.io/1v1-lol-2/'
-    
-    // Fetch the original URL
-    const response = await fetch(targetUrl, {
-        method: request.method,
-        headers: request.headers
-    })
-
-    // Return the response back to the user
-    return response
-}
+// Set the source of the iframe to the target URL
+document.getElementById('gameFrame').src = 'https://1v1lolonline.github.io/1v1-lol-2/';
